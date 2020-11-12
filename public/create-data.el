0e402757ac00b07aa380379caae388f8673e1a17
Xu Chunyang <xuchunyang56@gmail.com>
2020-11-13 01:05:34 +0800

    Generate major modes

--- public/create-data.el
+++ public/create-data.el
@@ -2,7 +2,18 @@
 
 (require 'json)
 
-(write-region
- (json-encode `((themes . ,(cons "default" (custom-available-themes)))))
- nil
- "data.json")
+(with-temp-buffer
+  (insert
+   (json-encode
+    `((themes . ,(cons "default" (custom-available-themes)))
+      (modes  . ,(seq-sort
+                  #'string<
+                  (seq-filter
+                   (lambda (name)
+                     (string-suffix-p "-mode" name))
+                   (delete-dups
+                    (mapcar #'symbol-name
+                            (seq-filter #'symbolp
+                                        (mapcar #'cdr auto-mode-alist))))))))))
+  (json-pretty-print-buffer-ordered)
+  (write-region nil nil "data.json"))
